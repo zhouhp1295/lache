a750b90cdf9f6d481b4a68a85c42aa83a75c091b
zhouhp1295 <554949297@qq.com>
2022-05-09 08:06:20 +0800

    使用 go 1.18

--- lache.go
+++ lache.go
@@ -1,191 +1,156 @@
 package lache
 
 import (
+	"reflect"
 	"sync"
 	"time"
 )
 
-var items map[string]*Item
+var items map[any]any
 var itemsRWMutex *sync.RWMutex
-var defaultIntervalItemOpts ItemOptions //默认的创建配置
 
 func init() {
 	//初始化读写锁
 	itemsRWMutex = new(sync.RWMutex)
-	//初始化默认选项
-	defaultIntervalItemOpts = ItemOptions{
-		Group:   DefaultGroup,
-		Mode:    Expire,
-		Expires: time.Hour,
-	}
 	//初始化map队列
-	items = make(map[string]*Item)
-	//开启timer
+	items = make(map[any]any)
+	//计时器
 	go tick()
 }
 
 func tick() {
-	interval := time.Second
-	timer := time.NewTimer(interval)
+	tickInterval := time.Second
+	timer := time.NewTimer(tickInterval)
+	var updatedAt int64
+	var interval, expires time.Duration
+	var mode ItemMode
 	for {
 		select {
 		case <-timer.C:
 			itemsRWMutex.Lock() //写锁
 			nano := time.Now().UnixNano()
 			for key, item := range items {
-				if item.Opts.Mode == Interval {
-					// 自动更新模式
-					if item.updatedAt+int64(item.Opts.Interval) < nano {
-						item.update()
-						pubItemEvent(key, ItemUpdate, *item)
-					}
-				} else if item.Opts.Mode == Expire {
-					// 过期模式
-					if item.updatedAt+int64(item.Opts.Expires) < nano {
-						pubItemEvent(key, ItemDelete, *item)
+				mode, _ = reflect.ValueOf(item).Elem().FieldByName("Mode").Interface().(ItemMode)
+				updatedAt, _ = reflect.ValueOf(item).Elem().FieldByName("UpdatedAt").Interface().(int64)
+				if mode == Expire { // 过期模式
+					expires, _ = reflect.ValueOf(item).Elem().FieldByName("Expires").Interface().(time.Duration)
+					if updatedAt+int64(expires) < nano {
+						pubItemEvent(key, ItemDelete, reflect.ValueOf(item).Elem().Interface())
+						ClearItemEvent(key)
 						delete(items, key)
+					}
+				} else if mode == Interval { // 定时更新模式
+					interval, _ = reflect.ValueOf(item).Elem().FieldByName("Interval").Interface().(time.Duration)
+					if updatedAt+int64(interval) < nano {
+						callRes := reflect.ValueOf(item).MethodByName("Update").Call(nil)
+						if len(callRes) > 0 {
+							if updateRes, ok := callRes[0].Interface().(bool); ok && updateRes {
+								pubItemEvent(key, ItemUpdate, reflect.ValueOf(item).Elem().Interface())
+							}
+						}
 					}
 				}
 			}
 			itemsRWMutex.Unlock() //释放写锁
-			timer.Reset(interval)
+			timer.Reset(tickInterval)
 		}
 	}
 }
 
-// NewDefaultItem 新建缓存项
-func NewDefaultItem(k string) (*Item, bool) {
-	return NewItem(k, defaultIntervalItemOpts)
-}
-
-// NewItem 新建缓存项
-func NewItem(k string, itemOpts ItemOptions) (*Item, bool) {
-	defer itemsRWMutex.Unlock() //释放写锁
-	itemsRWMutex.Lock()         //写锁
-	if _, exist := items[k]; exist {
-		return nil, false
-	}
-	nano := time.Now().UnixNano()
-	item := &Item{
-		Opts:      itemOpts,
-		key:       k,
-		createdAt: nano,
-		updatedAt: nano,
-		rwMutex:   new(sync.RWMutex),
-	}
-	if itemOpts.Mode == Interval && itemOpts.IntervalHandler != nil {
-		item.value = itemOpts.IntervalHandler()
-	}
-	items[k] = item
-	pubItemEvent(k, ItemCreate, *item)
-	return item, true
-}
-
-// NewItemWithOption 新建缓存项
-func NewItemWithOption(k string, opts ...Option) (*Item, bool) {
-	defer itemsRWMutex.Unlock() //释放写锁
-	itemsRWMutex.Lock()         //写锁
-	if _, exist := items[k]; exist {
-		return nil, false
-	}
-	itemOpts := defaultIntervalItemOpts
-	for _, opt := range opts {
-		opt(&itemOpts)
-	}
-	nano := time.Now().UnixNano()
-	item := &Item{
-		Opts:      itemOpts,
-		key:       k,
-		createdAt: nano,
-		updatedAt: nano,
-		rwMutex:   new(sync.RWMutex),
-	}
-	if itemOpts.Mode == Interval && itemOpts.IntervalHandler != nil {
-		item.value = itemOpts.IntervalHandler()
-	}
-	items[k] = item
-	pubItemEvent(k, ItemCreate, *item)
-	return item, true
-}
-
 // Set 设置kv
-func Set(k, v string, expires time.Duration) bool {
-	defer itemsRWMutex.Unlock() //释放写锁
-	itemsRWMutex.Lock()         //写锁
-	if _, exist := items[k]; exist {
-		return false
-	}
-	nano := time.Now().UnixNano()
-	item := &Item{
-		rwMutex: new(sync.RWMutex),
-		Opts: ItemOptions{
-			Mode:    Expire,
-			Group:   DefaultGroup,
-			Expires: expires,
-		},
-		createdAt: nano,
-		updatedAt: nano,
-		key:       k,
-		value:     v,
-	}
-	items[k] = item
-	pubItemEvent(k, ItemCreate, *item)
-	return true
+func Set(k string, v string, expires time.Duration) bool {
+	return SetKV[string, string](k, v, expires)
 }
 
-// Get 获取
+// Get 获取v
 func Get(k string) string {
+	return GetKV[string, string](k)
+}
+
+// Update 更新v
+func Update(k, v string) bool {
+	return UpdateKV[string, string](k, v)
+}
+
+// SetKV 设置kv
+func SetKV[K any, V any](k K, v V, expires time.Duration) bool {
+	_, ok := NewItemKV[K, V](k, v, WithExpires(expires))
+	return ok
+}
+
+// GetKV 获取
+func GetKV[K any, V any](k K) (v V) {
 	defer itemsRWMutex.RUnlock() //释放读锁
 	itemsRWMutex.RLock()         //读锁
 	item, exist := items[k]
 	if !exist {
-		return ""
+		return
 	}
-	return item.GetString()
+	values := reflect.ValueOf(item).MethodByName("Get").Call(nil)
+	if len(values) > 0 {
+		v, _ = values[0].Interface().(V)
+	}
+	return
 }
 
-// Update 更新
-func Update(k, v string) bool {
+// UpdateKV 更新
+func UpdateKV[K any, V any](k K, v V) bool {
 	defer itemsRWMutex.RUnlock() //释放读锁
 	itemsRWMutex.RLock()         //读锁
 	item, exist := items[k]
 	if !exist {
 		return false
 	}
-	item.Set(v)
-	pubItemEvent(k, ItemUpdate, *item)
+	reflect.ValueOf(item).MethodByName("Set").Call([]reflect.Value{reflect.ValueOf(v)})
+	pubItemEvent(k, ItemUpdate, reflect.ValueOf(item).Elem().Interface())
 	return true
 }
 
 // Delete 删除
-func Delete(k string) {
+func Delete(k any) {
 	defer itemsRWMutex.Unlock() //释放写锁
 	itemsRWMutex.Lock()         //写锁
 	if v, ok := items[k]; ok {
-		pubItemEvent(k, ItemDelete, *v)
+		pubItemEvent(k, ItemDelete, reflect.ValueOf(v).Elem().Interface())
 		delete(items, k)
+		ClearItemEvent(k)
 	}
-
 }
 
-// Clear 清空
-func Clear() {
+// NewItemKV 新的缓存项
+func NewItemKV[K any, V any](k K, v V, opts ...OptionFunc) (item *Item[K, V], ok bool) {
 	defer itemsRWMutex.Unlock() //释放写锁
 	itemsRWMutex.Lock()         //写锁
-	for k, v := range items {
-		pubItemEvent(k, ItemDelete, *v)
+	if _, exist := items[k]; exist {
+		return
 	}
-	items = make(map[string]*Item)
-}
+	nano := time.Now().UnixNano()
 
-// DeleteGroup 根据分组删除
-func DeleteGroup(group string) {
-	defer itemsRWMutex.Unlock() //释放写锁
-	itemsRWMutex.Lock()         //写锁
-	for k, v := range items {
-		if v.Opts.Group == group {
-			pubItemEvent(k, ItemDelete, *v)
-			delete(items, k)
-		}
+	defaultOpts := ItemOptions{
+		Mode:     Expire,
+		Group:    DefaultGroup,
+		Interval: time.Hour,
+		Expires:  time.Hour,
 	}
+	for _, opt := range opts {
+		opt(&defaultOpts)
+	}
+	item = &Item[K, V]{
+		key:        K(k),
+		value:      V(v),
+		Group:      defaultOpts.Group,
+		Mode:       defaultOpts.Mode,
+		Expires:    defaultOpts.Expires,
+		Interval:   defaultOpts.Interval,
+		updateFunc: defaultOpts.UpdateFunc,
+		CreatedAt:  nano,
+		UpdatedAt:  nano,
+		rwMutex:    new(sync.RWMutex),
+	}
+	items[k] = item
+	if item.Mode == Interval || item.Mode == Manual {
+		item.Update()
+	}
+	pubItemEvent(k, ItemCreate, *item)
+	return
 }
--- lache_test.go
+++ lache_test.go
@@ -1,40 +1,49 @@
 package lache
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
 func TestNewItem(t *testing.T) {
-	onCreated := ItemEventHandler(func(k string, event ItemEvent, item Item) {
-		t.Log("onEvent, k=", k, ",event=", event, ",item.value=", item.value)
+	onEvent := ItemEventHandler(func(k any, event ItemEvent, item interface{}) {
+		if item0, ok := item.(Item[string, string]); ok {
+			t.Logf("onEvent, k= %v, event = %v, item = %v", k, event, item0.Get())
+		}
 	})
-	onUpdated := ItemEventHandler(func(k string, event ItemEvent, item Item) {
-		t.Log("onEvent, k=", k, ",event=", event, ",item.value=", item.value)
+	onEvent2 := ItemEventHandler(func(k any, event ItemEvent, item interface{}) {
+		if item0, ok := item.(Item[string, string]); ok {
+			t.Logf("onEvent2, k= %v, event = %v, item = %v", k, event, item0.Get())
+		}
 	})
-	onDeleted := ItemEventHandler(func(k string, event ItemEvent, item Item) {
-		t.Log("onEvent, k=", k, ",event=", event, ",item.value=", item.value)
-	})
-	SubItemEvent("k01", ItemCreate, &onCreated)
-	SubItemEvent("k01", ItemUpdate, &onUpdated)
-	SubItemEvent("k01", ItemDelete, &onDeleted)
-	SubItemEvent("k02", ItemDelete, &onDeleted)
+	SubItemEvent("k01", ItemCreate, &onEvent)
+	SubItemEvent("k01", ItemDelete, &onEvent)
+	SubItemEvent("k01", ItemUpdate, &onEvent)
+	SubItemEvent("k02", ItemUpdate, &onEvent)
+	SubItemEvent("k02", ItemUpdate, &onEvent2)
+	SubItemEvent("k02", ItemDelete, &onEvent)
+	SubItemEvent("k04", ItemUpdate, &onEvent)
+	SubItemEvent("k04", ItemDelete, &onEvent)
+	Set("k01", "test01", time.Second*2)
 
-	Set("k02", "test", time.Second*2)
+	NewItemKV[string, string]("k02", "0", WithMode(Interval), WithInterval(time.Second), WithUpdateFunc(func() any {
+		return fmt.Sprintf("%d", time.Now().Second())
+	}))
+	//NewItemKV[string, string]("k02", "0", WithMode(Interval), WithInterval(time.Second))
 
-	NewItem("k01", ItemOptions{
-		Mode:     Interval,
-		Group:    DefaultGroup,
-		Interval: time.Second,
-		IntervalHandler: func() interface{} {
-			return time.Now().Second()
-		},
-	})
+	Set("k04", "test04", time.Hour)
+	t.Log(Get("k04"))
+	Update("k04", "test04-update")
+
 	go func() {
-		time.Sleep(time.Second * 3)
-		UnsubItemEvent("k01", ItemUpdate, &onUpdated)
-		time.Sleep(time.Second * 10)
-		Delete("k01")
+		time.Sleep(time.Second * 2)
+		UnsubItemEvent("k02", ItemUpdate, &onEvent)
+		Update("k04", "test04-update-update")
+		time.Sleep(time.Second * 5)
+		Delete("k02")
+		time.Sleep(time.Second * 2)
 	}()
-	time.Sleep(time.Second * 30)
+
+	time.Sleep(time.Second * 15)
 }
